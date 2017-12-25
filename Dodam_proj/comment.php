0e2b1aefd1f6c6953ad31dad59abfa2832f7a84b
gayeon1 <917sz914@gmail.com>
2017-12-25 15:52:21 +0900

    25일 15시

--- Dodam_proj/comment.php
+++ Dodam_proj/comment.php
@@ -1,13 +1,40 @@
 <?php
-   error_reporting(E_ALL);
-   ini_set("display_errors", 1);
-   require_once("dbconfig.php");
-?>
-<?php
-   $number = (int)$_GET['count']+1;
-   $sql = 'insert into comment values('.$number.',"'.$_GET['comment'].'",'.$_GET['id'].','.$_GET['point'].',"'.$_GET['name'].'","'.$_GET['password'].'",'.$_GET['islogin'].')';
-   echo $sql;
-   $db->query($sql);
-   $prevPage = $_SERVER['HTTP_REFERER'];
-   header('location:'.$prevPage);
+    $servername = "27.116.81.3";
+    $username = "hwadmin";
+    $password = "1234";
+    $dbname = "festivalhw";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+     }
+
+    session_start();
+         if(!isset($_SESSION['login_user'])){
+              echo "<meta http-equiv='refresh' content='0; url=login.php'>";
+         }
+
+         else {
+               $user = $_SESSION['login_user'];
+
+
+
+	$id = $_GET['festid'];
+	$content = $_GET['com'];
+	$datetime = date("Y-m-d H:i:s");
+
+
+
+    $sql = "INSERT INTO comment (comment, festival_id, comment_name, comment_datetime) VALUES ('$content', '$id', '$user', '$datetime')";
+    $result = mysqli_query($conn, $sql);
+
+	$prevPage = $_SERVER['HTTP_REFERER'];
+
+	header('location:'.$prevPage);
+
+
+}
 ?>
