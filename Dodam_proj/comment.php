78c5af3c2509402df3dbbd94ab8c536c8ef904ad
gayeon1 <917sz914@gmail.com>
2017-12-25 12:58:53 +0900

    25일 13시

--- Dodam_proj/comment.php
+++ Dodam_proj/comment.php
@@ -1,13 +1,13 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set("display_errors", 1);
-	require_once("dbconfig.php");
+   error_reporting(E_ALL);
+   ini_set("display_errors", 1);
+   require_once("dbconfig.php");
 ?>
 <?php
-	$number = (int)$_GET['count']+1;
-	$sql = 'insert into comment values('.$number.',"'.$_GET['comment'].'",'.$_GET['id'].','.$_GET['point'].',"'.$_GET['name'].'","'.$_GET['password'].'",'.$_GET['islogin'].')';
-	echo $sql;
-	$db->query($sql);
-	$prevPage = $_SERVER['HTTP_REFERER'];
-	header('location:'.$prevPage);
+   $number = (int)$_GET['count']+1;
+   $sql = 'insert into comment values('.$number.',"'.$_GET['comment'].'",'.$_GET['id'].','.$_GET['point'].',"'.$_GET['name'].'","'.$_GET['password'].'",'.$_GET['islogin'].')';
+   echo $sql;
+   $db->query($sql);
+   $prevPage = $_SERVER['HTTP_REFERER'];
+   header('location:'.$prevPage);
 ?>
--- Dodam_proj/like.php
+++ Dodam_proj/like.php
@@ -1,25 +1,49 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set("display_errors", 1);
-	require_once("dbconfig.php");
-?>
-<?php
-    session_start();
+    $servername = "27.116.81.3";
+    $username = "hwadmin";
+    $password = "1234";
+    $dbname = "festivalhw";
 
-    if(!isset($_SESSION['login_user'])){
-         echo "<meta http-equiv='refresh' content='0; url=login.php'>";
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+     }
+
+    session_start();
+         if(!isset($_SESSION['login_user'])){
+              echo "<meta http-equiv='refresh' content='0; url=login.php'>";
+         }
+
+         else {
+               $user = $_SESSION['login_user'];
+
+
+
+	$id = $_POST['id'];
+	$datetime = date("Y-m-d H:i:s");
+
+    $sql = "select * from heart where like_guestID = '$user' and like_festID = '$id'";
+    $result = mysqli_query($conn, $sql);
+    $result = mysqli_num_rows($result);
+
+    if(!$result) {
+      $sql = "insert into heart (like_guestID, like_festID, like_datetime) VALUES ('$user', '$id', '$datetime')";
+    $sql2 = "update festival set like_count = like_count + 1 where festival_id = '$id' ";
     }
 
     else {
-          $user = $_SESSION['login_user'];
+        $sql = "UPDATE festival SET like_count = like_count - 1 WHERE festival_id = '$id' ";
+        		$sql2 = "DELETE FROM heart WHERE like_guestID = '$user' and like_festID = '$id'";
     }
 
-	$id = $_GET['id'];
-
-	$sql = 'update festival set like_count = like_count + 1 where festival_id='.$id;
-	$db->query($sql);
+	mysqli_query($conn, $sql);
+	mysqli_query($conn, $sql2);
 
 	$prevPage = $_SERVER['HTTP_REFERER'];
 
 	header('location:'.$prevPage);
+}
 ?>
